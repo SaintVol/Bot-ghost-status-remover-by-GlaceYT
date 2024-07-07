f96a267efb8e2e8b3ff4947252181700a061fbf9
SaintVol <162009870+SaintVol@users.noreply.github.com>
2024-07-07 17:14:34 +0800

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Vascos British Army!", "Made by Saintvol"];
+const statusMessages = ["Watching Hood T!", "Made by Saintvol","Hood T on top!];
 
 
 let currentIndex = 0;
